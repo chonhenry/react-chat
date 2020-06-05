d0cbee71f451d04d02446dc6866e20e5f7ebadb0
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-06-04 22:38:55 -0400

    setup redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import firebase from "./firebase/firebase";
+// import firebase from "./firebase/firebase";
+import reducers from "./store/reducers/index";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
